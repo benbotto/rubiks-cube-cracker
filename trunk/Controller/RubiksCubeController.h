d65a255b5e7341ff88ab57451e3f831f3a1df38b
avejidah <avejidah@f3177daf-26d8-4cec-8a05-a2d535385721>
2014-05-03 18:18:59 +0000

    Working cube with a basic controller and view.

---
+++ trunk/Controller/RubiksCubeController.h
@@ -0,0 +1,41 @@
+#ifndef _BUSYBIN_RUBIKS_CUBE_CONTROLLER_H_
+#define _BUSYBIN_RUBIKS_CUBE_CONTROLLER_H_
+
+#include "../Model/RubiksCube.h"
+#include "../View/RubiksCubeView.h"
+#include <map>
+using std::map;
+#include <string>
+using std::string;
+#include <sstream>
+using std::istringstream;
+#include <functional>
+using std::function;
+using std::bind;
+#include <algorithm>
+using std::copy;
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cin;
+using std::getline;
+
+namespace busybin
+{
+  class RubiksCubeController
+  {
+    typedef function<void()>        moveFunc_t;
+    typedef map<string, moveFunc_t> moveMap_t;
+
+    RubiksCube     cube;
+    RubiksCubeView view;
+    moveMap_t      moveMap;
+
+  public:
+    RubiksCubeController();
+    void start();
+  };
+}
+
+#endif
+
