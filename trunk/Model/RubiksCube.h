4ff8ae2aa77af323468cb3964256fed13f916add
avejidah <avejidah@f3177daf-26d8-4cec-8a05-a2d535385721>
2014-05-22 01:01:15 +0000

    Added cubies to RubiksCube.

--- trunk/Model/RubiksCube.h
+++ trunk/Model/RubiksCube.h
@@ -4,9 +4,15 @@
 #include "../Util/RubiksCubeException.h"
 #include <array>
 using std::array;
+#include <set>
+using std::set;
+#include <map>
+using std::map;
 #include <algorithm>
 using std::fill;
 using std::next;
+using std::set_intersection;
+using std::next_permutation;
 #include <iterator>
 using std::advance;
 
@@ -19,14 +25,31 @@ namespace busybin
     enum class COLOR : char {WHITE = 'W', GREEN  = 'G', RED    = 'R',
                              BLUE  = 'B', ORANGE = 'O', YELLOW = 'Y'};
 
-  private:
-    array<COLOR, 54> cube;
+    typedef COLOR           CenterCubie;
+    typedef array<COLOR, 2> EdgeCubie;
+    typedef array<COLOR, 3> CornerCubie;
 
+  private:
+    typedef map<FACE, unsigned> centerMap;
+    typedef map<array<FACE, 2>, array<unsigned, 2> > edgeMap;
+    typedef map<array<FACE, 3>, array<unsigned, 3> > cornerMap;
+
+    array<COLOR, 54> cube;
+    centerMap        centerCubies;
+    edgeMap          edgeCubies;
+    cornerMap        cornerCubies;
+
+    void permuteEdge(array<FACE, 2> faces, array<unsigned, 2> indices);
+    void permuteCorner(array<FACE, 3> faces, array<unsigned, 3> indices);
   public:
     RubiksCube();
 
     COLOR get(unsigned i) const;
     COLOR get(FACE face, unsigned row, unsigned col) const;
+
+    CenterCubie getCubie(FACE f1) const;
+    EdgeCubie   getCubie(FACE f1, FACE f2) const;
+    CornerCubie getCubie(FACE f1, FACE f2, FACE f3) const;
 
     RubiksCube& u();
     RubiksCube& uPrime();
