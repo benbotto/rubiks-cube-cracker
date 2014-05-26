b985497c9e858edf1e4c76077aac05e19c68bf62
avejidah <avejidah@f3177daf-26d8-4cec-8a05-a2d535385721>
2014-05-26 18:47:51 +0000

    2 goals in place - adding a UI.

--- trunk/Controller/RubiksCubeController.cpp
+++ trunk/Controller/RubiksCubeController.cpp
@@ -18,13 +18,29 @@ namespace busybin
     RubiksCubeView cubeView;
     MoveStore      moveStore(cube);
     Scrambler      scrambler(cube, moveStore, cubeView);
-    BBSolver       solver(cube);
+    CubeSearcher   searcher(cube);
+    vector<string> moves;
+    Goal1          goal1;
+    Goal2          goal2;
 
     // Let the user scramble the cube.
     scrambler.manualScramble();
 
-    // Solve the cube.
-    solver.solve();
+    // Try to achieve the goals.
+    searcher.find(goal1, moves);
+    searcher.find(goal2, moves);
+
+    // Print the moves.
+    for (string move : moves)
+      cout << move << ' ';
+    cout << endl;
+
+    for (string move : moves)
+    {
+      cout << move << endl;
+      moveStore.getMoveFunc(move)();
+      cubeView.render(cube);
+    }
   }
 }
 
