c4a92f9d25c93d60c147a21c3599a737e6fb44d0
avejidah <avejidah@f3177daf-26d8-4cec-8a05-a2d535385721>
2014-06-06 19:35:45 +0000

    Partial. Goal6 started.

---
+++ trunk/Model/Goal/Goal6.h
@@ -0,0 +1,20 @@
+#ifndef _BUSYBIN_GOAL6_H_
+#define _BUSYBIN_GOAL6_H_
+
+#include "Goal.h"
+#include "../RubiksCubeModel.h"
+
+namespace busybin
+{
+  /**
+   * Goal 6 - 2x2x3 cube with an additional edges and corner solved.
+   */
+  class Goal6 : public Goal
+  {
+  public:
+    bool isSatisfied(RubiksCubeModel& cube);
+  };
+}
+
+#endif
+
