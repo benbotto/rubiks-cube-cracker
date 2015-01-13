f547759bf5bdb3d7db1a42c9491ddb9313253cd9
avejidah <avejidah@f3177daf-26d8-4cec-8a05-a2d535385721>
2015-01-13 19:26:26 +0000

    Cube dumper fix - only dumps on key 'press'.

--- branches/bitboard/Controller/Command/CubeDumper.cpp
+++ branches/bitboard/Controller/Command/CubeDumper.cpp
@@ -35,7 +35,7 @@ namespace busybin
    */
   void CubeDumper::onKeypress(int key, int scancode, int action, int mods)
   {
-    if (key == GLFW_KEY_SPACE)
+    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
       this->view.render(this->pCube->getRawModel());
   }
 }
