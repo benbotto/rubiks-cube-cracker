0e54f86bcdf8f46b19ee8cc882e2cbf1e8226fa7
avejidah <avejidah@f3177daf-26d8-4cec-8a05-a2d535385721>
2015-01-17 16:04:19 +0000

    Reduced the number of moves in the G1 Twist Store.  Had to remove one of the obvious prunes accordingly.

--- branches/bitboard/Model/MoveStore/ModelG1TwistStore.cpp
+++ branches/bitboard/Model/MoveStore/ModelG1TwistStore.cpp
@@ -8,24 +8,14 @@ namespace busybin
    *        string->function map.
    */
   ModelG1TwistStore::ModelG1TwistStore(RubiksCubeModel& cube) :
-    TwistStore(vector<string>
-    ({{
-      "L", "L'", "L2",
-      "R", "R'", "R2",
-                 "U2",
-                 "D2",
-      "F", "F'", "F2",
-      "B", "B'", "B2"
-    }}))
+    TwistStore(vector<string>({{"L", "R", "U2", "D2", "F", "B"}}))
   {
     // Set up the move map.
     this->moveMap["L"]  = bind(&RubiksCubeModel::l,      &cube);
     this->moveMap["L'"] = bind(&RubiksCubeModel::lPrime, &cube);
-    this->moveMap["L2"] = bind(&RubiksCubeModel::l2,     &cube);
 
     this->moveMap["R"]  = bind(&RubiksCubeModel::r,      &cube);
     this->moveMap["R'"] = bind(&RubiksCubeModel::rPrime, &cube);
-    this->moveMap["R2"] = bind(&RubiksCubeModel::r2,     &cube);
 
     this->moveMap["U2"] = bind(&RubiksCubeModel::u2,     &cube);
 
@@ -33,11 +23,9 @@ namespace busybin
 
     this->moveMap["F"]  = bind(&RubiksCubeModel::f,      &cube);
     this->moveMap["F'"] = bind(&RubiksCubeModel::fPrime, &cube);
-    this->moveMap["F2"] = bind(&RubiksCubeModel::f2,     &cube);
 
     this->moveMap["B"]  = bind(&RubiksCubeModel::b,      &cube);
     this->moveMap["B'"] = bind(&RubiksCubeModel::bPrime, &cube);
-    this->moveMap["B2"] = bind(&RubiksCubeModel::b2,     &cube);
   }
 
   /**
