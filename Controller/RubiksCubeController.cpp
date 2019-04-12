e58a62aeb5011111117da79206621045862eab4b
benbotto <benbotto@users.noreply.github.com>
2019-04-11 18:11:02 -0700

    CHORE: Spacing cleanup.

--- Controller/RubiksCubeController.cpp
+++ Controller/RubiksCubeController.cpp
@@ -10,13 +10,13 @@ namespace busybin
     try
     {
       // Pass width and height for windowed-mode.
-      WorldWindow                   worldWnd("Rubik's Cube", 1024, 768);
-      RubiksCubeWorld               world(unique_ptr<Program>(new RubiksCubeProgram()));
-      ViewManager                   viewMan(&world,    &worldWnd);
-      Renderer                      renderer(&world,   &worldWnd);
-      CubeMover                     cubeMover(&world,  &worldWnd);
-      ThistlethwaiteCubeSolver      tCubeSolver(&world, &worldWnd, &cubeMover);
-      CubeDumper                    cubeDumper(&world, &worldWnd);
+      WorldWindow              worldWnd("Rubik's Cube", 1024, 768);
+      RubiksCubeWorld          world(unique_ptr<Program>(new RubiksCubeProgram()));
+      ViewManager              viewMan(&world,    &worldWnd);
+      Renderer                 renderer(&world,   &worldWnd);
+      CubeMover                cubeMover(&world,  &worldWnd);
+      ThistlethwaiteCubeSolver tCubeSolver(&world, &worldWnd, &cubeMover);
+      CubeDumper               cubeDumper(&world, &worldWnd);
 
       // All wired up, run the main loop.
       worldWnd.run();
