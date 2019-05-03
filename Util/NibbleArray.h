817831f7332aae1b4f60af3ab3039bfa6f5442f0
Ben Botto <benjamin.botto@gmail.com>
2019-05-03 12:17:52 -0700

    Adds a method to inflate an array for faster access.

--- Util/NibbleArray.h
+++ Util/NibbleArray.h
@@ -5,6 +5,7 @@
 using std::size_t;
 #include <vector>
 using std::vector;
+#include <cstdint>
 
 namespace busybin
 {
@@ -13,19 +14,21 @@ namespace busybin
    */
   class NibbleArray
   {
-    typedef unsigned char uchar;
+    size_t size;
 
     // This is where the data are stored.  It's a vector rather than an array
     // because the OS imposes stack size limitations, and very large arrays are
     // needed to hold the pattern databases.
-    vector<uchar> arr;
+    vector<uint8_t> arr;
+
   public:
-    NibbleArray(const size_t size, const uchar val = 0xFF);
-    uchar get(const size_t pos) const;
-    void set(const size_t pos, const uchar val);
+    NibbleArray(const size_t size, const uint8_t val = 0xFF);
+    uint8_t get(const size_t pos) const;
+    void set(const size_t pos, const uint8_t val);
     unsigned char* data();
     const unsigned char* data() const;
     size_t storageSize() const;
+    void inflate(vector<uint8_t>& dest) const;
   };
 }
 
