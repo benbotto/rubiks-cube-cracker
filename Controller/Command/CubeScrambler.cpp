8bafb967453ce3d06900e2858a4dc0487a12eba3
Ben Botto <benjamin.botto@gmail.com>
2019-05-21 20:16:05 -0700

    New scramble command (F5).

---
+++ Controller/Command/CubeScrambler.cpp
@@ -0,0 +1,76 @@
+#include "CubeScrambler.h"
+#include <functional>
+using std::bind;
+using std::ref;
+using std::placeholders::_1;
+using std::placeholders::_2;
+using std::placeholders::_3;
+using std::placeholders::_4;
+
+namespace busybin
+{
+  /**
+   * Init, storing a reference to the cube.
+   * @param pWorld Pointer to the world (must remain in scope).
+   * @param pWorldWnd The world window, for binding keypress events.
+   * @param pMover Pointer to the a CubeMover instance, which is used to apply
+   * the scramble moves.
+   */
+  CubeScrambler::CubeScrambler(World* pWorld, WorldWindow* pWorldWnd, CubeMover* pMover) : 
+    Command(pWorld, pWorldWnd),
+    pMover(pMover)
+  {
+    // Listen for keypress events.
+    pWorldWnd->onKeypress(bind(&CubeScrambler::onKeypress, ref(*this), _1, _2, _3, _4));
+
+    this->pCube = dynamic_cast<RubiksCubeWorldObject*>(&pWorld->at("RubiksCube"));
+  }
+
+  /**
+   * Fires when a key is pressed and scrambles the cube.
+   * @param key The key code.
+   * @param scancode The platform-dependent scan code of the key.
+   * @param action GLFW_PRESS, GLFW_RELEASE, GLFW_REPEAT.
+   * @param mods Modifiers like alt.
+   */
+  void CubeScrambler::onKeypress(int key, int scancode, int action, int mods)
+  {
+    // On F5 press, and if movement is enabled (e.g. not solving or
+    // initializing).
+    if (key == GLFW_KEY_F5 && action == GLFW_PRESS && this->pMover->isEnabled())
+    {
+      typedef RubiksCube::MOVE MOVE;
+
+      Random       rand(0, 17);
+      MovePruner   pruner;
+      vector<MOVE> moves;
+
+      moves.reserve(100);
+      moves.push_back((MOVE)rand.next());
+
+      while (moves.size() < 100)
+      {
+        MOVE move = (MOVE)rand.next();
+
+        if (!pruner.prune(moves.back(), move))
+          moves.push_back(move);
+      }
+
+      cout << "Scramble: ";
+      cout << setfill(' ');
+
+      for (unsigned i = 0; i < moves.size(); ++i)
+      {
+        if (i % 10 == 0)
+          cout << '\n';
+
+        cout << setw(3) << left << this->pCube->getMove(moves.at(i));
+      }
+      cout << endl;
+
+      for (const MOVE move : moves)
+        this->pCube->move(move);
+    }
+  }
+}
+
---
+++ Controller/Command/CubeScrambler.h
@@ -0,0 +1,36 @@
+#ifndef _BUSYBIN_CUBE_SCRAMBLER_H_
+#define _BUSYBIN_CUBE_SCRAMBLER_H_
+
+#include "CubeMover.h"
+#include "../../OpenGLSeed/Controller/Command/Command.h"
+#include "../../Model/RubiksCube.h"
+#include "../../Model/WorldObject/RubiksCubeWorldObject.h"
+#include "../Searcher/MovePruner.h"
+#include "../../Util/Random.h"
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <iomanip>
+using std::setw;
+using std::setfill;
+using std::left;
+
+namespace busybin
+{
+  /**
+   * A command that scrambles the cube on keypress.
+   */
+  class CubeScrambler : public Command
+  {
+    RubiksCubeWorldObject* pCube;
+    CubeMover*             pMover;
+
+  public:
+    CubeScrambler(World* pWorld, WorldWindow* pWorldWnd, CubeMover* pMover);
+    void onKeypress(int key, int scancode, int action, int mods);
+  };
+}
+
+#endif
