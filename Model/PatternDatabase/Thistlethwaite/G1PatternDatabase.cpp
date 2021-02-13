b46ba1d3321079699cec7bc3d40036c2d40cbaa8
Ben Botto <benjamin.botto@gmail.com>
2021-02-12 18:04:15 -0800

    Adds a pattern database for G1.

---
+++ Model/PatternDatabase/Thistlethwaite/G1PatternDatabase.cpp
@@ -0,0 +1,41 @@
+#include "G1PatternDatabase.h"
+
+namespace busybin
+{
+  /**
+   * Initialize the database storage.  There are 12 edges, two orientations for
+   * each, and the orientations of 11 dictate the orientation of the 12th (due
+   * to even edge parity).  There are then 2^11 orientations, and the database
+   * needs 2^11 / 1024^2 / 2 = 1KB on disk.
+   */
+  G1PatternDatabase::G1PatternDatabase() : PatternDatabase(2048)
+  {
+  }
+
+  /**
+   * Given a cube, get an index into the pattern database.
+   */
+  uint32_t G1PatternDatabase::getDatabaseIndex(const RubiksCube& cube) const
+  {
+    typedef RubiksCubeIndexModel::EDGE EDGE;
+
+    const RubiksCubeIndexModel& iCube = static_cast<const RubiksCubeIndexModel&>(cube);
+
+    // Treat the orientations as a base-2 number, and convert it to base-10.
+    // Because 11 edges dictate the orientation of the 12th, only 11 edges
+    // matter in this calculation.
+    return
+      iCube.getEdgeOrientation(EDGE::UB) * 1024 +
+      iCube.getEdgeOrientation(EDGE::UR) * 512 +
+      iCube.getEdgeOrientation(EDGE::UF) * 256 +
+      iCube.getEdgeOrientation(EDGE::UL) * 128 +
+      iCube.getEdgeOrientation(EDGE::FR) * 64 +
+      iCube.getEdgeOrientation(EDGE::FL) * 32 +
+      iCube.getEdgeOrientation(EDGE::BL) * 16 +
+      iCube.getEdgeOrientation(EDGE::BR) * 8 +
+      iCube.getEdgeOrientation(EDGE::DF) * 4 +
+      iCube.getEdgeOrientation(EDGE::DL) * 2 +
+      iCube.getEdgeOrientation(EDGE::DB);
+  }
+}
+
---
+++ Model/PatternDatabase/Thistlethwaite/G1PatternDatabase.h
@@ -0,0 +1,27 @@
+#ifndef _BUSYBIN_G1_PATTERN_DATABASE_H_
+#define _BUSYBIN_G1_PATTERN_DATABASE_H_
+
+#include "../PatternDatabase.h"
+#include "../../RubiksCube.h"
+#include "../../RubiksCubeIndexModel.h"
+#include "../PatternDatabase.h"
+#include <cstdint>
+#include <array>
+using std::array;
+
+namespace busybin
+{
+  /**
+   * A pattern database that holds the orientations of the 12 edges.
+   * Used for moving from G0->G1 using the Thistlethwaite algorithm.
+   */
+  class G1PatternDatabase : public PatternDatabase
+  {
+  public:
+    G1PatternDatabase();
+    uint32_t getDatabaseIndex(const RubiksCube& cube) const;
+  };
+}
+
+#endif
+
