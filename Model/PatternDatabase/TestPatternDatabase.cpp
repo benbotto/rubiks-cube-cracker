c7f0c89db4d7a5d7f31342d0aab9966d4faaa82f
Ben Botto <benjamin.botto@gmail.com>
2021-02-28 09:33:54 -0800

    Changes M- to E-slice edges.

--- Model/PatternDatabase/TestPatternDatabase.cpp
+++ Model/PatternDatabase/TestPatternDatabase.cpp
@@ -3,7 +3,7 @@
 namespace busybin
 {
   /**
-   * All arrangements of the 8 edge cubies in the E and S slices are reachable,
+   * All arrangements of the 8 edge cubies in the M and S slices are reachable,
    * so there are 8! = 40320 permutations.
    */
   TestPatternDatabase::TestPatternDatabase() : PatternDatabase(40320)
@@ -19,33 +19,33 @@ namespace busybin
 
     const RubiksCubeIndexModel& iCube = static_cast<const RubiksCubeIndexModel&>(cube);
 
-    // This maps the edges in the E and S slices to sequential indexes.  Edges
-    // in the M slice are ignored since they cannot be moved out of the M slice
-    // with G2 twists (no quarter turns of F or B).
+    // This maps the edges in the M and S slices to sequential indexes.  Edges
+    // in the E slice are ignored since they cannot be moved out of the E slice
+    // with G2 twists (no quarter turns of F/B/L/R).
     array<uint8_t, 12> edgeMap;
 
-    //edgeMap[(unsigned)EDGE::UB] = 15;
-    edgeMap[(unsigned)EDGE::UR] = 0;
-    //edgeMap[(unsigned)EDGE::UF] = 15;
-    edgeMap[(unsigned)EDGE::UL] = 1;
-    edgeMap[(unsigned)EDGE::FR] = 2;
-    edgeMap[(unsigned)EDGE::FL] = 3;
-    edgeMap[(unsigned)EDGE::BL] = 4;
-    edgeMap[(unsigned)EDGE::BR] = 5;
-    //edgeMap[(unsigned)EDGE::DF] = 15;
-    edgeMap[(unsigned)EDGE::DL] = 6;
-    //edgeMap[(unsigned)EDGE::DB] = 15;
+    edgeMap[(unsigned)EDGE::UB] = 0;
+    edgeMap[(unsigned)EDGE::UR] = 1;
+    edgeMap[(unsigned)EDGE::UF] = 2;
+    edgeMap[(unsigned)EDGE::UL] = 3;
+    //edgeMap[(unsigned)EDGE::FR] = 15;
+    //edgeMap[(unsigned)EDGE::FL] = 15;
+    //edgeMap[(unsigned)EDGE::BL] = 15;
+    //edgeMap[(unsigned)EDGE::BR] = 15;
+    edgeMap[(unsigned)EDGE::DF] = 4;
+    edgeMap[(unsigned)EDGE::DL] = 5;
+    edgeMap[(unsigned)EDGE::DB] = 6;
     edgeMap[(unsigned)EDGE::DR] = 7;
 
     array<uint8_t, 8> edgeIndexes =
     {
+      edgeMap[iCube.getEdgeIndex(EDGE::UB)],
       edgeMap[iCube.getEdgeIndex(EDGE::UR)],
+      edgeMap[iCube.getEdgeIndex(EDGE::UF)],
       edgeMap[iCube.getEdgeIndex(EDGE::UL)],
-      edgeMap[iCube.getEdgeIndex(EDGE::FR)],
-      edgeMap[iCube.getEdgeIndex(EDGE::FL)],
-      edgeMap[iCube.getEdgeIndex(EDGE::BL)],
-      edgeMap[iCube.getEdgeIndex(EDGE::BR)],
+      edgeMap[iCube.getEdgeIndex(EDGE::DF)],
       edgeMap[iCube.getEdgeIndex(EDGE::DL)],
+      edgeMap[iCube.getEdgeIndex(EDGE::DB)],
       edgeMap[iCube.getEdgeIndex(EDGE::DR)]
     };
 
